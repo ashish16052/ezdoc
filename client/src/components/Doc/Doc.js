46794b2f0824082da004f5b07f5261d48fae7045
ashish <ashishmohite16052@gmail.com>
2023-03-25 11:39:29 +0530

    Title: Added quill text editor changes: Added quill library for editing text in the document

--- client/src/components/Doc/Doc.js
+++ client/src/components/Doc/Doc.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
 import './Doc.scss'
 
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, false] }, { font: [] }],
+    [],
+    ['bold', 'italic', 'underline', 'strike'],
+    [],
+    [{ color: [] }, { background: [] }],
+    [],
+    [{ list: 'ordered' }, { list: 'bullet' }, { align: [] }],
+    [],
+    ['link', 'image', 'blockquote', 'code-block'],
+]
+
 const Doc = () => {
-  return (
-    <div>Doc</div>
-  )
+    const [quill, setQuill] = useState()
+
+    const wrapperRef = useCallback(wrapper => {
+        if (!wrapper) return
+        wrapper.innerHTML = ""
+        const editor = document.createElement("div")
+        wrapper.append(editor)
+        const q = new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } })
+        setQuill(q)
+    }, [])
+
+    return (
+        <div className='Doc'>
+            <div className='sidebar'>
+                <h2>Insert Chart</h2>
+                <p>Histogram</p>
+                <p>Pie chart</p>
+                <p>Confusion matrix</p>
+            </div>
+            <div className='Container' ref={wrapperRef}></div>
+        </div>
+    )
 }
 
 export default Doc
